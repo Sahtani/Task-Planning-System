e89a3ef479f0433280c88d43670f3445c1f6b261
soumia <soumia.sahtani@gmail.com>
2023-12-23 23:15:50 +0100

    add structure mvc

---
+++ app/data/Db.php
@@ -0,0 +1,23 @@
+<?php
+class Db {
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $dbname = 'dataware1'; 
+    public function connect(){
+        try {
+                $conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+               
+                $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+               
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+                 
+                return $conn;
+             
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+    }
+}
+?>
+
---
+++ app/helpers/session_helpers.php
@@ -0,0 +1,8 @@
+ <?php   function isUserLogged() {
+    if(isset($_SESSION["authorize"]) && $_SESSION["authorize"]) {
+    return true;
+    }else {
+    return false;
+    }
+    }
+    ?>
